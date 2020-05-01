543f91a2fb54a5d792653610411a59c75b992a90
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-05-01 15:59:57 +0200

    Added exercises/exercise-1.js.

--- exercises/exercise-1.js
+++ exercises/exercise-1.js
@@ -1,110 +1,84 @@
-// counting cards
-var count = 0;
+// replacing if else chains with switch
 
-function cc(card) {
+function chainToSwitch(val) {
+  var answer = "";
   // Only change code below this line
-  var regex = /[JQKA]/;
-  if (card > 1 && card < 7) {
-    count++;
-  } else if (card === 10 || String(card).match(regex)) {
-    count--;
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
   }
 
-  if (count > 0) return count + " Bet";
-  return count + " Hold";
-}
-
-cc(2);
-cc(3);
-cc(7);
-cc('K');
-cc('A');
-
-// Global scope and functions
-
-// Declare the myGlobal variable below this line
-var myGlobal = 10
-
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-  oopsGlobal = 5
-}
-
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-
-  //Global scope vs local scope in functions
-
-  // Setup
-  var outerWear = "T-Shirt";
-
-  function myOutfit() {
-    // Only change code below this line
-    var outerWear = "sweater"
-    return outerWear;
-
-    // Only change code above this line
-    return outerWear;
-  }
-
-  myOutfit();
-
-  // replacing if else chains with switch
-
-  function chainToSwitch(val) {
-    var answer = "";
-    // Only change code below this line
-
-    switch (val) {
-      case "bob":
-        answer = "Marley";
-        break;
-      case 42:
-        answer = "The Answer";
-        break;
-      case 1:
-        answer = "There is no #1";
-        break;
-      case 99:
-        answer = "Missed me by this much!";
-        break;
-      case 7:
-        answer = "Ate Nine";
-        break;
-    }
-
-    // Only change code above this line
-    return answer;
-  }
-
-  chainToSwitch(7);
-}
-
-
-// stand in line
-
-function nextInLine(arr, item) {
-  // Only change code below this line
-  arr.push(item)
-  return arr.shift(item);
   // Only change code above this line
-
-
+  return answer;
 }
+
+chainToSwitch(7);
+}
+
+//Record collection.
 
 // Setup
-var testArr = [1, 2, 3, 4, 5];
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: []
+  },
+  5439: {
+    album: "ABBA Gold"
+  }
+};
 
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
+// Only change code below this line
+function updateRecords(id, prop, value) {
+
+  if (prop === "tracks" && collection[id][prop] === undefined) {
+    collection[id][prop] = [];
+  } else if (prop === "tracks" && value !== "") {
+    collection[id][prop].push(value)
+  } else if (prop !== "tracks" && value !== "") {
+    collection[id][prop] = value
+  } else if (value === "") {
+    delete collection[id][prop]
+  }
+  return collection;
+
+}
+
+updateRecords(5439, "artist", "ABBA");
+updateRecords(5439, "tracks", "Take a Chance on Me");
+updateRecords(1245, "album", "Riptide")
+
+// 1. if prop is tracks but the element does not have the property, create the property of the pair(by an empty array)
+// 2. if prop is tracks and value isn't empty, push the value onto the end of the album existing tracks array.
+// 3. if prop isnt tracks and value isnt blank, update or set the value for that record album's property.
+// 4. if value is blank, delete that prop from the album
